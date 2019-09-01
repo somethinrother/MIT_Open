25b09b5a4aa7c8338c9f58c6a2e9e8bc895cd78b
somethinrother <jamesneilson4@gmail.com>
2019-08-31 22:07:27 -0400

    Refactoring exercise class

--- reading_2.java
+++ reading_2.java
@@ -1,8 +1,18 @@
-public class Thing {
+public class ReadingTwo {
   public static void main(String[] args) {
+    exerciseOne();
+    exerciseTwo();
+  }
+
+  private static void exerciseOne() {
     int number = 4;
-    double wowo = 3.14;
-    System.out.print(number + "\n");
-    System.out.print(wowo + "\n\n\n\n\n\n\n\n\n\n\n\n");
+    System.out.println("EXERCISE #1");
+    System.out.println(number);
+  }
+
+  private static void exerciseTwo() {
+    int result = ((((1 + 2 - 1) * 2) / 2) + 8) % 7;
+    System.out.println("EXERCISE #2");
+    System.out.println(result);
   }
 }
