80a2bdee463509a5dafeef81584311feee611170
somethinrother <jamesneilson4@gmail.com>
2019-08-31 22:16:36 -0400

    Third exercise done

--- reading_2.java
+++ reading_2.java
@@ -2,6 +2,7 @@ public class ReadingTwo {
   public static void main(String[] args) {
     exerciseOne();
     exerciseTwo();
+    exerciseThree();
   }
 
   private static void exerciseOne() {
@@ -14,5 +15,18 @@ public class ReadingTwo {
     int result = ((((1 + 2 - 1) * 2) / 2) + 8) % 7;
     System.out.println("EXERCISE #2");
     System.out.println(result);
+  }
+
+  private static void exerciseThree() {
+    System.out.println("EXERCISE #3");
+    int aNumber = 3;
+    if (aNumber >= 0) {
+      if (aNumber == 0) {
+        System.out.println("first string");
+      } else {
+        System.out.println("second string");
+      }
+
+    System.out.println("third string");
   }
 }
