577c93b2d3640debebb150f856e27a0793a84113
somethinrother <jamesneilson4@gmail.com>
2019-08-31 21:59:07 -0400

    Get exercise 1 working

---
+++ reading_2.java
@@ -0,0 +1,8 @@
+public class Thing {
+  public static void main(String[] args) {
+    int number = 4;
+    double wowo = 3.14;
+    System.out.print(number + "\n");
+    System.out.print(wowo + "\n\n\n\n\n\n\n\n\n\n\n\n");
+  }
+}
